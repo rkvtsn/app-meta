597b3d69364be320e8de5df1d8adf06a40cb3007
Andrey Rukavitsyn <rkvtsn@gmail.com>
2023-01-12 16:11:50 +0100

    [assets] optimized images

--- src/components/Image/Images.js
+++ src/components/Image/Images.js
@@ -1,5 +1,5 @@
-import Bruchetta from "../../assets/bruchetta.svg";
-import GreekSalad from "../../assets/greek salad.jpg";
+import Bruchetta from "../../assets/bruchetta 1.png";
+import GreekSalad from "../../assets/greek salad 1.png";
 import LemonDessert from "../../assets/lemon dessert.jpg";
 import Restauranfood from "../../assets/restauranfood.jpg";
 
